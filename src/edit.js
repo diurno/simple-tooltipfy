18db15d90fd08f733baf4386834936bf51f5f589
federico cadierno <diurnodiez@gmail.com>
2021-08-31 23:12:23 -0400

    Add files via upload

---
+++ src/edit.js
@@ -0,0 +1,55 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+
+ import { __ } from '@wordpress/i18n';
+ import {  InnerBlocks, useBlockProps, RichText } from '@wordpress/block-editor';
+ 
+ 
+ /**
+  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+  * Those files can contain any CSS code that gets applied to the editor.
+  *
+  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+  */
+ import './editor.scss';
+ 
+ /**
+  * The edit function describes the structure of your block in the context of the
+  * editor. This represents what the editor will render when the block is used.
+  *
+  * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+  *
+  * @param {Object} [props]           Properties passed from the editor.
+  * @param {string} [props.className] Class name generated for the block.
+  *
+  * @return {WPElement} Element to render.
+  */
+ export default function Edit( { attributes, setAttributes } ) {
+ 
+	 const blockProps = useBlockProps();
+
+	 return (
+		 <>
+			 <RichText
+                { ...blockProps }
+                tagName="p"
+				className= "fede"
+				role="tooltip"
+				aria-label= {attributes.toolTipText}
+				data-microtip-position={attributes.toolTipPosition}
+				style={{"--tooltip-color": attributes.toolTipColor, "--tooltip-opacity": attributes.toolTipOpacity}}
+                onChange={ ( newContent ) => {
+					setAttributes( {
+						content: newContent,
+					} );
+				} }
+                value={ attributes.content }
+            />
+		 </>
+		 
+	 );
+ }
+ 
---
+++ src/index.js
@@ -0,0 +1,68 @@
+/**
+ * Registers a new block provided a unique name and an object defining its behavior.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * All files containing `style` keyword are bundled together. The code used
+ * gets applied both to the front of your site and to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './style.scss';
+
+/**
+ * Internal dependencies
+ */
+import Edit from './edit';
+import save from './save';
+import './filter/extendBlock';
+import icons from './icons/icons.js'
+
+
+/**
+ * Every block starts by registering a new block type definition.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+registerBlockType( 'federico-cadierno/simple-tooltipfy', {
+
+	description: __(
+		'Display extra information using the fancy simple tooltipfy ',
+		'list-with-tooltip'
+	),
+
+	/**
+	 * Blocks are grouped into categories to help users browse and discover them.
+	 * The categories provided by core are `common`, `embed`, `formatting`, `layout` and `widgets`.
+	 */
+	category: 'text',
+
+	/**
+	 * An icon property should be specified to make it easier to identify a block.
+	 * These can be any of WordPress’ Dashicons, or a custom svg element.
+	 */
+	icon: icons.toolTipNew,
+
+	attributes: {
+        content: {
+            type: 'array',
+            source: 'children',
+            selector: 'p',
+        },
+    },
+
+	/**
+	 * @see ./edit.js
+	 */
+	edit: Edit,
+
+	/**
+	 * @see ./save.js
+	 */
+	save,
+} );
